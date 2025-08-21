d71bbfa6156dd933ae22b4869dc9eebc39ff77df
RazanAli456 <sultanaiqbal88@gmail.com>
2025-08-21 18:47:07 +0500

    initial comit

---
+++ Python Lesson 2/index.py
@@ -0,0 +1,11 @@
+name="RAZAN" #string
+
+age=10 #integer
+
+student=True #boolean
+
+weight=24.5 #float
+print("My Age Is",age)
+print("My Name Is",name)
+print("My Weight Is",weight)
+print("I Am A",student)
