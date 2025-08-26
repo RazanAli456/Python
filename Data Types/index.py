467dbe68f6b64af3ab21db9149512b60cc0f909e
RazanAli456 <sultanaiqbal88@gmail.com>
2025-08-26 18:36:47 +0500

    commit

---
+++ Data Types/index.py
@@ -0,0 +1,18 @@
+a=5
+b=("hello")
+c=True
+d=67.98
+
+print("The Data Type Of a Is",type(a))
+print("The Data Type Of b Is",type(b))
+print("The Data Type Of c Is",type(c))
+print("The Data Type Of d Is",type(d))
+age=input("enter your age")
+print("The Data Type Of Age Is",type(age))
+#Learning To type cast
+age=int(age)
+print("The Data Type Of Age Is",type(age))
+R=input("Do You Like To Go To School")
+print("The Data Type Of R Is",type(R))
+R=bool(R)
+print("The Data Type Of R Is",type(R))
