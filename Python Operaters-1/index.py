885f24690483f1063cb68ff8ead5afb55eb2f183
RazanAli456 <sultanaiqbal88@gmail.com>
2025-08-28 18:34:37 +0500

    commit

---
+++ Python Operaters-1/index.py
@@ -0,0 +1,11 @@
+#There are five trees in Jack's front yard.
+#He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper.
+#Jack's list: 98, 94, 41, 96, and 11.
+#What is the average height of a tree in Jack's front yard?
+R=98
+F=94
+Z=41
+S=96
+H=11
+avg_of_trees=(R+F+Z+S+H)/5
+print("average of all trees are",avg_of_trees)
