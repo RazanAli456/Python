ca4c693eea8e3f68ab850868092392af1ab961a1
RazanAli456 <sultanaiqbal88@gmail.com>
2025-08-28 18:53:57 +0500

    commit

---
+++ Python Operaters-1/percantage.py
@@ -0,0 +1,8 @@
+science=int(input("Enter the marks of science"))
+urdu=int(input("Enter the marks of urdu"))
+math=int(input("Enter the marks of math"))
+english=int(input("Enter the marks of english"))
+sum_of_marks=math=english=urdu=science
+sum_of_marks=(science+math+urdu+english)/400
+percentage=sum_of_marks*100
+print("The Percantage Of Yours Is",percentage)
