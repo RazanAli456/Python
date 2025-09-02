e9a92a402682c693d84c4510185e0dee89b3f926
RazanAli456 <sultanaiqbal88@gmail.com>
2025-09-02 18:42:15 +0500

    commit

---
+++ Conditanal Statement/index2.py
@@ -0,0 +1,8 @@
+#Write a program to check whether the given number is greater than 15 or smaller than 15.
+num=int(input("Enter A number"))
+if num>15:
+    print("The Number Is Greater Than 15")
+elif num<15:
+    print("The Number Is Smaller Than 15")
+else:
+    print("The Number Might Be Greater Or Smaller Than 15...")
