6d9277e1d369050d8a0854d8c7f20df9e6c435ae
RazanAli456 <sultanaiqbal88@gmail.com>
2025-09-02 18:51:22 +0500

    commit

---
+++ Conditanal Statement/index3.py
@@ -0,0 +1,6 @@
+#Write a program to check whether the given number is even or odd?
+num=int(input("Enter A Number To Check If It Is Even Or Odd"))
+if num%2==0:
+    print("Even")
+else :
+    print("Odd")    
