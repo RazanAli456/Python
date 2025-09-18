9a2704afa52339503433544d611d7dc8762c5bd3
RazanAli456 <sultanaiqbal88@gmail.com>
2025-09-18 18:08:31 +0500

    commit

---
+++ Summer Times/index.py
@@ -0,0 +1,7 @@
+print("What Is The Temprature")
+Temp=int(input("Pls Enter The Temprature"))
+
+if Temp>30:
+    print("The Wheather Is Hot")
+elif Temp<30:
+    print("The Wheather Is A Little Cold")
