7485f4099ee6ac9304509840f36baa31b0f83c03
RazanAli456 <sultanaiqbal88@gmail.com>
2025-10-07 17:24:36 +0500

    commit

---
+++ Square/Main.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+turtle.Screen().title("Drawing A Square")
+Draw=turtle.Turtle()
+
+Draw.forward(100)
+Draw.right(90)
+Draw.forward(100)
+Draw.right(90)
+Draw.forward(100)
+Draw.right(90)
+Draw.forward(100)
+Draw.right(90)
+
+turtle.done()
