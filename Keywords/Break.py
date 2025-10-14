5141dd13842b3e566535a8cf71f150fa8bf30ce7
RazanAli456 <sultanaiqbal88@gmail.com>
2025-10-14 18:43:08 +0500

    commit

---
+++ Keywords/Break.py
@@ -0,0 +1,8 @@
+word=input("Enter A Word To Check If The Letter 'A' Is In The Word'")
+
+for i in word:
+    if i=='A':
+        print("The Alphabet 'A' Is In This Word")
+        break
+    else:
+        print(i)
---
+++ Keywords/Return.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter a number"))
+num2=int(input("Enter another number"))
+def add(num1,num2):
+    return
+num1+num2
+sum=add(num1,num2)
+print("The sum of the two numbers is",sum)
