7bacb11eb05ca22feaad7feb70842fa6e8ba7558
RazanAli456 <sultanaiqbal88@gmail.com>
2025-10-14 17:46:07 +0500

    commit

---
+++ Shut Down/Main.py
@@ -0,0 +1,6 @@
+sd=(input("Do You Want To Shut Down Your Computer?"))
+
+if sd=="yes":
+    print("Click The Windows Icon And Press Shut Down")
+else:
+    print("Ok You Can Shut Down Your Computer Next Time")
