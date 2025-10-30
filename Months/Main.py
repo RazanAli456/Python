688c21f6a0e8d12734c111c2a87b22db6959bf05
RazanAli456 <sultanaiqbal88@gmail.com>
2025-10-30 17:41:41 +0500

    Commit

---
+++ Months/Main.py
@@ -0,0 +1,6 @@
+Month=input("Do You Wanna Know The Names Of The Months?")
+
+if Month=="yes":
+    print("These Are The Name,January,Febuary,March,April,May,June,July,August,September,October,November,December")
+else :
+    print("You Can Come Here Again If You Wanna Know")
